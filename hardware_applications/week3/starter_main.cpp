275b0ed737841b4d517eb612cf9d2c83f60654d1
Jianni Adkisson <jadkisson6@gatech.edu>
2017-10-12 19:51:28 -0400

    Week 3 update

--- hardware_applications/week3/starter_main.cpp
+++ hardware_applications/week3/starter_main.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 #include <STSL/RJRobot.h>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
 
     RJRobot robot;
+    vector<int> measurements;
+    auto black_threshold = 10;
+    auto grey_threshold = 40;
 
+    while(measurements.empty() || measurements.back() > black_threshold) {
+        // Measure square color
+        measurements.push_back(robot.LightValue());
+
+        // Move to next square
+        robot.SetMotor(MotorPort::A, 200);
+        robot.SetMotor(MotorPort::B, 200);
+        robot.Wait(250ms);
+    }
 
 
     return 0;
